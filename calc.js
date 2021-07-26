105eea6fdc5a6b61a97397a57f6d330b1ef8543a
Sarah Drew <drew.sarah.e@gmail.com>
2021-07-26 14:03:42 -0700

    added operations to calculator

--- calc.js
+++ calc.js
@@ -14,6 +14,34 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	let solution;
+
+	if (mathSymbol === "+"){
+		solution = num1 + num2;
+	}
+	else if (mathSymbol === "-"){
+		solution = num1 - num2;
+	}
+	else if (mathSymbol === "*"){
+		solution = num1 * num2;
+	}
+	else if (mathSymbol === "/"){
+		solution = num1 / num2;
+	}
+	else if (mathSymbol === "v"){
+		solution = Math.sqrt(num1);
+	}
+	else if (mathSymbol === "^"){
+		solution = num1 ** num2;
+	}
+	else if (mathSymbol === "%"){
+		solution = num1 % num2;
+	}
+	else{
+		solution = "You must've typed something wrong, because I cannot calculate this.";
+	}
+
+	console.log(solution);
 
 	// This line closes the connection to the command line interface.
 	reader.close()
